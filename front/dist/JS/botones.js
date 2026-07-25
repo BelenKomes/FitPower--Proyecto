de5e1122ad352c799d558e0dde6bb2ad453deb9c
Ana Belén Komés Vaucher <280184371+BelenKomes@users.noreply.github.com>
2026-07-25 06:09:27 -0300

    Eliminación de botones.js

--- front/dist/JS/botones.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".botón-más-información").forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        document.querySelector(`#desc-${e.target.dataset.desc}`).classList.toggle("oculto");
-    })
-})
